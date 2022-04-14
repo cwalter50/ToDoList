7b4ba5a66744341eb5360838805b4fe81607b0c9
Chris Walter <walt50@gmail.com>
2022-04-14 19:34:41 -0400

    added App Icon

--- ToDoList/View/ListView.swift
+++ ToDoList/View/ListView.swift
@@ -14,7 +14,7 @@ struct ListView: View {
     var body: some View {
         ZStack {
             if listViewModel.items .isEmpty {
-                NoItemsView()
+                NoItemsView().transition(AnyTransition.opacity.animation(.easeInOut))
             }
             else {
                 List {
